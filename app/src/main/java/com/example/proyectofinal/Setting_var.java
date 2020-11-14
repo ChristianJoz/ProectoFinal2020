412a4c047fbd27c5b0ee7fa5f806e9fb8fbe94a4
Karla703 <64883489+Karla703@users.noreply.github.com>
2020-11-13 23:12:29 -0600

    Funciones de registro de categoria

---
+++ app/src/main/java/com/example/proyectofinal/Setting_var.java
@@ -0,0 +1,11 @@
+package com.example.proyectofinal;
+
+public class Setting_var {
+    public static final String URL_Eliminar_categoria = "http://pinguox.6te.net/AppInventario/EliminarCategoria.php";
+
+    public static final String URL_guardar_categoria = "http://pinguox.6te.net/AppInventario/guardar_categoria.php";
+
+    public static final String URL_consultaAllCategorias = "http://pinguox.6te.net/AppInventario/buscar_categoria.php";
+
+    public static final String URL_Editar_Categoria =  "http://pinguox.6te.net/AppInventario/EditarCategoria.php";
+}
